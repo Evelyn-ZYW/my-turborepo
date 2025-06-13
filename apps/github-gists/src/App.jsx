efbf5d7c32632d7ff1a9ee323e3a65f424f5cd38
Evie Zhao <evelyn.helloworld@gmail.com>
2025-06-13 12:08:38 -0700

    create InfiniteList

--- apps/github-gists/src/App.jsx
+++ apps/github-gists/src/App.jsx
@@ -1,9 +1,11 @@
 import PaginatedList from "./comps/PaginatedList";
+import InfiniteList from "./comps/InfiniteList";
 import "./App.scss";
 
 function App() {
   return <div className="App">
   <PaginatedList />
+  <InfiniteList />
   </div>;
 }
 
